5f80d1e0c81487e834fe87269c9da5e205267266
LJW0316 <lijingwei010316@163.com>
2022-04-30 21:21:44 +0800

    init

---
+++ course_select-back_end/src/main/java/com/course_selectback_end/CourseSelectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.course_selectback_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSelectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSelectBackEndApplication.class, args);
+    }
+
+}
---
+++ course_select-back_end/src/test/java/com/course_selectback_end/CourseSelectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course_selectback_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
